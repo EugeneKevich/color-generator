2f06ee7ba706f507dffb251432fc3d167e54b709
Eugene Stashkevych <eugene.kevich@gmail.com>
2022-09-06 22:11:23 +0200

    update color-generator

--- index.js
+++ index.js
@@ -1,16 +1,31 @@
-let myName = 'Eugene';
-console.log(myName);
+// node process
+import { argv } from 'node:process';
+// npm package
+import chalk from 'chalk';
+import randomColor from 'randomcolor';
 
-const hashArray = [
-  '################################',
-  '################################',
-  '################################',
-  '################################',
-  '################################',
-  '################################',
-  '################################',
-  '################################',
-  '################################',
-];
+let hexCode;
+const color = process.argv[2];
+const saturated = process.argv[3];
+const log = console.log;
 
-console.log(hashArray);
+if (process.argv[2] && process.argv[3]) {
+  hexCode = randomColor({ hue: color, luminosity: saturated });
+} else {
+  hexCode = randomColor({ hue: color });
+}
+
+const element = `  ################################
+  ################################
+  ################################
+  #####                      #####
+  #####       ${hexCode}        #####
+  #####                      #####
+  ################################
+  ################################
+  ################################`;
+
+log(chalk.hex(hexCode)(element));
+// console.log(argv[2], argv[3]);
+
+// console.log(randomColor(myName));
